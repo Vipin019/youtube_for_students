0e1610a5f942a5026fa24b09c59276177fae5a03
Vipin019 <82204019+Vipin019@users.noreply.github.com>
2022-03-06 14:56:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var menuIcon=document.querySelector(".hamburger");
 var sidebar=document.querySelector(".sidebar");
+var main=document.querySelector(".main");
 menuIcon.onclick = function () {
     sidebar.classList.toggle("small-sidebar");
+    main.classList.toggle("large-main");
+}
+var searchIcon=document.querySelector(".search-btn");
+
+var main=document.querySelector(".main");
+searchIcon.onclick = function () {
+    
+    main.classList.toggle("notfound-main");
 }
